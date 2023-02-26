2fa660b4bd3697c099da2720cd24902d24457c5f
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-02-26 16:42:14 +0000

    passing data through props

--- src/App.js
+++ src/App.js
@@ -2,20 +2,24 @@ export default function Board() {
   return (
     <>
       <div className="boardRow">
-        <button className="square">1</button>
-        <button className="square">2</button>
-        <button className="square">3</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className="boardRow">
-        <button className="square">4</button>
-        <button className="square">5</button>
-        <button className="square">6</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className="boardRow">
-        <button className="square">7</button>
-        <button className="square">8</button>
-        <button className="square">9</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   );
+}
+
+function Square(props) {
+  return <button className="square">{props.value}</button>;
 }

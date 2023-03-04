8e19221025a7ad6a107d712dc5db2c2080c5c8c3
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-03-04 17:50:57 +0000

    functionality added to show past moves

--- src/App.js
+++ src/App.js
@@ -150,6 +150,24 @@ export default function Game() {
     setHistory([...history, nextSquares]);
     setXIsNext(!xIsNext);
   }
+
+  function jumpTo(nextMove) {
+    // TODO
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = `Go to move #${move}`;
+    } else {
+      description = `Go to game start`;
+    }
+    return (
+      <li>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  })
   
   return (
     <div className="game">
@@ -158,7 +176,7 @@ export default function Game() {
       </div>
       <div className="game-info">
         <ol>
-          {/*to-do*/}
+          {moves}
         </ol>
       </div>
     </div>

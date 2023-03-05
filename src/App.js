53fd3e5fedc21086db1feafa25e5d4d0bd219071
Georgia E. Whitney <81175151+rockyrelay@users.noreply.github.com>
2023-03-05 22:50:53 +0000

    functionality to display move history complete

--- src/App.js
+++ src/App.js
@@ -141,20 +141,25 @@ export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   // keep track of which step user is viewing
   const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove]
 
   // read last squares array from the history
-  const currentSquares = history[history.length - 1];
+  // const currentSquares = history[history.length - 1];
 
   // will be called by board component to update the game
   function handlePlay(nextSquares) {
     // creates new array containing all history items
     // spread syntax as "enumerate all items in history"
-    setHistory([...history, nextSquares]);
+    // setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
     setXIsNext(!xIsNext);
   }
 
   function jumpTo(nextMove) {
-    // TODO
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
